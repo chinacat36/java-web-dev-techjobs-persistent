05694e4cc2397fa75604a49bc0d4d38559769d0a
Nicole Holder <nholder06@gmail.com>
2020-04-06 12:20:14 -0500

    Added dependencies to build.gradle along with the application.properties.  Also updated classes and created repositories for Skill and Employer classes.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @OneToOne(cascade = CascadeType.ALL)
+    private String location;
 
+    public Employer(){};
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
